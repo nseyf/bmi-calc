da5aa3348c9df04c851dbd82fcbade3aa3df3fce
Nima Seyf <nimaseyf@gmail.com>
2017-06-29 13:30:17 +0100

    modified css

--- src/App.js
+++ src/App.js
@@ -8,34 +8,44 @@ class App extends Component {
     this.state = {
       height: "",
       weight: "",
-      result: ""
+      result: "",
+      message: ""
     }
   }
 
+
+
 updateHeight(e) {
-  this.setState({height: e.target.value ? parseInt(e.target.value): ""})
+
+  this.setState({
+    height: e.target.value ? parseInt(e.target.value): ""
+  });
+
 }
 
 updateWeight(e) {
-  this.setState({weight: e.target.value ? parseInt(e.target.value): ""})
+  this.setState({weight: e.target.value ? parseInt(e.target.value): ""});
+
 }
+// BMI is weight / height(in m) then / by height again
+
 
 
   render() {
 
 const inputDiv = {
  margin: "50px",
-
+fontWeight: "bold"
 }
 const titleStyle = {
   fontSize: "50px",
-  color: "#151515"
+  color: "#151515",
+  fontWeight: "900"
 }
 const containerStyle = {
-  background: "white",
   padding: "50px",
-  marginTop: "40px",
-  fontFamily: "'Fredericka the Great', cursive",
+  background: "white",
+  fontFamily: "'Roboto', cursive",
   boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
 }
 const identifierStyle = {
@@ -46,15 +56,13 @@ const identifierStyle = {
       console.log(this.state)
     return (
 
-      <div className="App" className="col-xs-12 col-lg-6 col-lg-offset-3 col-md-6 text-center" style={containerStyle}>
-        <h1 style={titleStyle}>BMI Calculator</h1>
-        <div style={inputDiv}><span style={identifierStyle}>Enter your Height: </span><input key={this.height} onChange={this.updateHeight.bind(this)} value ={this.state.height} />
+      <div className="App" className="col-xs-12 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 text-center" style={containerStyle}>
+        <div><h1 style={titleStyle}>BMI Calculator</h1></div>
+        <div style={inputDiv}><span style={identifierStyle}>Enter your Height(cm): </span><input key={this.height} onChange={this.updateHeight.bind(this)} value={this.state.height} />
       </div>
-      <div style={inputDiv}><span style={identifierStyle}>Enter your Weight: </span><input key={this.weight} onChange={this.updateWeight.bind(this)} value={this.state.weight}/>
+      <div style={inputDiv}><span style={identifierStyle}>Enter your Weight(kg): </span><input key={this.weight} onChange={this.updateWeight.bind(this)} value={this.state.weight}/>
       </div>
       <div>
-        <h2>Your BMI is: {this.state.result}</h2>
-        <h2>This classifies you as Obese</h2>
       </div>
       <div>
 
