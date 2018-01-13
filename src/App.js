195475404da4dad1d7c1453db48c905e34df46f2
Nima Seyf <nimaseyf@gmail.com>
2018-01-13 11:15:48 +0000

    improved code indents

--- src/App.js
+++ src/App.js
@@ -17,43 +17,57 @@ class App extends Component {
       message: ""
     }
 
-this.renderMessage = this.renderMessage.bind(this);
+this.renderBMIClassMessage = this.renderBMIClassMessage.bind(this);
+this.updateUserHeight = this.updateUserHeight.bind(this);
+this.updateUserWeight = this.updateUserWeight.bind(this);
   }
 
 
-updateHeight(e) {
+updateUserHeight(e) {
   // Updates state with user input for their height converted to a decimal (base 10).
   this.setState({
     height: e.target.value ? parseInt(e.target.value, 10): ""
-  }, () => {
-// Once state has been update with the height value, setState is then called
-// to calculate the BMI. As the app uses CM for height input state height is
-// converted to metres.
-    this.setState({result: this.state.weight / (this.state.height / 100) / (this.state.height / 100)}, () => {
-// Finally, the renderMessage function is called to classify the result BMI score within the categories.
-      this.renderMessage();
-    });
-});
-}
+        }, () => {
+        // Once state has been update with the height value, setState is then called
+        // to calculate the BMI. As the app uses CM for height input state height is
+        // converted to metres.
+       this.setState({
+           result: this.state.weight / (this.state.height / 100) / (this.state.height / 100)
+                }, () => {
+                  // Finally, the renderMessage function is called to classify
+                  // the result BMI score within the categories.
+                       this.renderBMIClassMessage();
+                     }
+                   )
+                 }
+               )
+             }
 
-updateWeight(e) {
-  // Updates state with user input converted to a decimal (base 10).
-  this.setState({weight: e.target.value ? parseInt(e.target.value, 10): ""}, () => {
-    // Once state has been update with the weight value, setState is then called
-    // to calculate the BMI. Once again height is converted to metres.
-    this.setState({result: this.state.weight / (this.state.height / 100) / (this.state.height / 100)}, () => {
-      // Finally, the renderMessage function is called to classify the result BMI score within the categories.
-      this.renderMessage();
-    })
-  });
-}
-
+updateUserWeight(e) {
+  // Updates state with user weight input converted to a decimal (base 10).
+  this.setState({
+    weight: e.target.value ? parseInt(e.target.value, 10): ""
+    }, () => {
+      // Once state has been update with the weight value, setState is then called
+      // to calculate the BMI. Once again height is converted to metres.
+      this.setState({
+        result: this.state.weight / (this.state.height / 100) / (this.state.height / 100)
+             }, () => {
+               // Finally, the renderMessage function is called to classify the result BMI score within the categories.
+                  this.renderBMIClassMessage();
+                }
+              )
+            }
+          )
+        }
 
 // In both updateHeight and updateWeight ternary operators are used
 // when setting state to ensure that the app does not crash trying to
 // calculate BMI with data it does not have.
 
-renderMessage(){
+
+
+renderBMIClassMessage(){
 const { result } = this.state;
 // BMI values within certain ranges indicate different classifications of physical health.
 // renderMessage takes the result from state then sets message state to a different string
@@ -61,25 +75,44 @@ const { result } = this.state;
 // displaying this information to the user.
 
   if(result < 15) {
+
     this.setState({message: "You are very severely underweight"})
+
   } else if (result > 15 && result < 16) {
+
     this.setState({message: "You are severely underweight"})
+
   } else if (result > 16 && result < 18.5) {
+
     this.setState({message: "You are underweight"})
+
   } else if (result > 18 && result < 25) {
+
     this.setState({message: "Your BMI is normal"})
+
   } else if (result > 25 && result < 30) {
+
     this.setState({message: "You are overweight"})
+
   } else if (result > 30 && result < 35) {
+
     this.setState({message: "You are moderately Obese"})
+
   } else if (result > 35 && result < 40) {
+
     this.setState({message: "You are severely obese"})
+
   } else if (result > 40) {
+
     this.setState({message: "You are very severely obese"})
   }
+
+
 }
 
   render() {
+
+// Styles
 
 const inputDiv = {
  margin: "50px"
@@ -105,18 +138,21 @@ backgroundImage: "linear-gradient(to right, #ff758c 0%, #ff7eb3 100%)",
 
     return (
 
-      <div className="App col-xs-12 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 text-center" style={containerStyle}>
-        <div><h1 style={titleStyle}>BMI CALCULATOR</h1></div>
+      <div className="App col-xs-12 col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 text-center"
+      style={containerStyle}>
+        <div>
+        <h1 style={titleStyle}>BMI CALCULATOR</h1>
+        </div>
         <div style={inputDiv}>
         <input style={inputStyle} placeholder="Enter your Height (cm)"
         key={this.height}
-        onChange={this.updateHeight.bind(this)}
+        onChange={this.updateUserHeight}
         value={this.state.height} />
       </div>
       <div style={inputDiv}>
       <input style={inputStyle} placeholder="Enter your Weight (kg)"
       key={this.weight}
-      onChange={this.updateWeight.bind(this)}
+      onChange={this.updateUserWeight}
       value={this.state.weight}/>
       </div>
       <div>
